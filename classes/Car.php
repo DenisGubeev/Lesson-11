75d4176fda6bcc99d8dc6c6b8cd897754e4d27fb
Denis <35633718+DenisGubeev@users.noreply.github.com>
2018-03-29 17:24:02 +0300

    Update Car.php

--- classes/Car.php
+++ classes/Car.php
@@ -4,32 +4,21 @@ namespace Namespaces;
 
 interface CarInterface
 {
-    public function isHasOil();
-    public function setOilVolume($volume);
+    public function setColor($color);
+    public function getColor();
 }
 class Car extends Product implements CarInterface
 {
-    protected $color = 'blue';
-    protected $tankSize = 50;
-    protected $oilVolume;
-    public function setOilVolume($volume)
+    private $color;
+    public function setColor($color)
     {
-        if ($volume > $this->tankSize) {
-           $this->oilVolume = $this->tankSize;
-        }
-        if ($volume < 0) {
-            $this->oilVolume = 0;
-        }
-        if ($volume < $this->tankSize) {
-            $this->oilVolume = $volume;
-        }
+      $this->color = $color;
+      return $this;
     }
-    public function isHasOil()
+    public function getColor()
     {
-        if ($this->oilVolume/$this->tankSize < 0.1) {
-            return false;
-        }
-        return true;
+        return $this->color;
     }
+	
 }
 ?>
